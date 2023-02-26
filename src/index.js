e908e691cd7ccde564e2da9072cc7b6064ed1fb3
Gilberto Jr <giljrsantos@gmail.com>
2023-02-26 17:21:08 -0300

    Tese unitário onde o teste será executado somente depois de alguns segundos que a pessoa ficar fora do site.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
+/**
+ * @class OnLeaveIntent
+ * @description A class to check if the user is leaving the window
+ * and run a callback when it happens.
+ * @param callback - a function to run when the user is leaving the page
+ * @param delay - time in milliseconds defined to start tracking the user
+ */
+
 export default class OnLeaveIntent {
-  constructor(callback) {
+  constructor(callback, delay) {
     this.callback = callback
+    this.delay = delay
 
     this.init()
   }
 
   init = () => {
-    this.handleMouseOut()
+    this.timer = window.setTimeout(this.handleMouseOut, this.delay)
+  }
+  destroy = () => {
+    clearTimeout(this.timer)
+    document.removeEventListener('mouseout', this.checkOutOfBounds)
   }
 
   checkOutOfBounds = e => {
     if (e.relatedTarget === null) {
       this.callback()
+      this.destroy()
     }
   }
 
-  handleMouseOut() {
+  handleMouseOut = () => {
     document.addEventListener('mouseout', this.checkOutOfBounds)
   }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -2,21 +2,43 @@ import OnLeaveIntent from './index'
 
 describe('OnLeaveIntent', () => {
   let callback
+  // eslint-disable-next-line no-unused-vars
   let onLeaveIntent
+
+  const delay = 1000 // 1000ms === 1s
+  jest.useFakeTimers()
 
   beforeEach(() => {
     callback = jest.fn()
-    onLeaveIntent = new OnLeaveIntent(callback)
+    onLeaveIntent = new OnLeaveIntent(callback, delay)
   })
-  
+
   it('should run the callback function if the user goes out of the screen', () => {
+    // advance 1s
+    jest.advanceTimersByTime(delay)
     // simulate the user leaving the page
     document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
     expect(callback).toHaveBeenCalled()
   })
 
   it('should not run the callback function if the user stills on the screen', () => {
+    jest.advanceTimersByTime(delay)
     document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: new EventTarget() }))
     expect(callback).not.toHaveBeenCalled()
+  })
+
+  it('should not run the callback function before the delay', () => {
+    // advance only .5s
+    jest.advanceTimersByTime(delay / 2)
+    document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
+    expect(callback).not.toHaveBeenCalled()
+  })
+
+  it('should call the callback function only once', () => {
+    jest.advanceTimersByTime(delay)
+    document.dispatchEvent(new MouseEvent('mouseout'))
+    document.dispatchEvent(new MouseEvent('mouseout'))
+
+    expect(callback).toHaveBeenCalledTimes(1)
   })
 })
