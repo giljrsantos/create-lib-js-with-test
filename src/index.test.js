5b6ae1b061b4543ef63929c6ffb9b52c1499886d
Gilberto Jr <giljrsantos@gmail.com>
2023-02-25 22:18:01 -0300

    Criando teste unitario para verificar o envento do mouse esta denro da tela, ou não.

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,23 @@
 import OnLeaveIntent from './index'
 
 describe('OnLeaveIntent', () => {
-  it('should return 5', () => {
-    expect(5).toBe(5)
+  it('should run the callback function if the user goes out of the screen', () => {
+    const callback = jest.fn()
+
+    const onLeaveIntent = new OnLeaveIntent(callback)
+
+    // simulate the user leaving the page
+    document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
+    expect(callback).toHaveBeenCalled()
+  })
+
+  it('should not run the callback function if the user stills on the screen', () => {
+    const callback = jest.fn()
+
+    const onLeaveIntent = new OnLeaveIntent(callback)
+
+    // simulate the user leaving the page
+    document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: new EventTarget() }))
+    expect(callback).not.toHaveBeenCalled()
   })
 })
